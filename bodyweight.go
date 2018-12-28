824d14e4b2c6762c99168a5c3229561b9dca3a14
Werner Schneider <werner@schneidernet.com>
2018-12-28 18:30:04 +0100

    test response

--- bodyweight.go
+++ bodyweight.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
+
+var result = `{
+	"version": "1.0",
+	"sessionAttributes": {},
+	"response": {
+	  "outputSpeech": {
+		"type": "PlainText",
+		"text": "Welcome to the Alexa Skills Kit sample. Please tell me your favorite color by saying, my favorite color is red"
+	  },
+	  "card": {
+		"type": "Simple",
+		"title": "SessionSpeechlet - Welcome",
+		"content": "SessionSpeechlet - Welcome to the Alexa Skills Kit sample. Please tell me your favorite color by saying, my favorite color is red"
+	  },
+	  "reprompt": {
+		"outputSpeech": {
+		  "type": "PlainText",
+		  "text": "Please tell me your favorite color by saying, my favorite color is red"
+		}
+	  },
+	  "shouldEndSession": false
+	}
+  }`
 
 type MyEvent struct {
 	Name string `json:"name"`
@@ -14,7 +36,8 @@ type MyEvent struct {
 
 func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
 	log.Printf("Context: %v\n\nEvent: %v\n", ctx, name)
-	return fmt.Sprintf("Hello Dear %s!", name.Name), nil
+	//	return fmt.Sprintf("Hello Dear %s!", name.Name), nil
+	return result, nil
 }
 
 func main() {
