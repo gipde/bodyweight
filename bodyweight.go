bd83bd06bec829fb4441b6a72f25298cf836b599
Werner Schneider <werner@schneidernet.com>
2018-12-28 19:08:54 +0100

    fix

--- bodyweight.go
+++ bodyweight.go
@@ -2,44 +2,52 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-var result = `{
-	"version": "1.0",
-	"sessionAttributes": {},
-	"response": {
-	  "outputSpeech": {
-		"type": "PlainText",
-		"text": "Welcome to the Alexa Skills Kit sample. Please tell me your favorite color by saying, my favorite color is red"
-	  },
-	  "card": {
-		"type": "Simple",
-		"title": "SessionSpeechlet - Welcome",
-		"content": "SessionSpeechlet - Welcome to the Alexa Skills Kit sample. Please tell me your favorite color by saying, my favorite color is red"
-	  },
-	  "reprompt": {
-		"outputSpeech": {
-		  "type": "PlainText",
-		  "text": "Please tell me your favorite color by saying, my favorite color is red"
-		}
-	  },
-	  "shouldEndSession": false
+var result = []byte(`{
+	"version":"1.0",
+	"sessionAttributes":{
+ 
+	},
+	"response":{
+	   "outputSpeech":{
+		  "type":"PlainText",
+		  "text":"Welcome to the Alexa Skills Kit sample. Please tell me your favorite color by saying, my favorite color is red"
+	   },
+	   "card":{
+		  "type":"Simple",
+		  "title":"SessionSpeechlet - Welcome",
+		  "content":"SessionSpeechlet - Welcome to the Alexa Skills Kit sample. Please tell me your favorite color by saying, my favorite color is red"
+	   },
+	   "reprompt":{
+		  "outputSpeech":{
+			 "type":"PlainText",
+			 "text":"Please tell me your favorite color by saying, my favorite color is red"
+		  }
+	   },
+	   "shouldEndSession":false
 	}
-  }`
+ }`)
 
 type MyEvent struct {
 	Name string `json:"name"`
 }
 
-func HandleRequest(ctx context.Context, name MyEvent) (string, error) {
+func HandleRequest(ctx context.Context, name MyEvent) (interface{}, error) {
 	log.Printf("Context: %v\n\nEvent: %v\n", ctx, name)
-	//	return fmt.Sprintf("Hello Dear %s!", name.Name), nil
-	return result, nil
+	var f interface{}
+	err := json.Unmarshal(result, &f)
+	return f, err
 }
 
 func main() {
+	// log.Printf("starting...")
+	// var f interface{}
+	// json.Unmarshal(result, &f)
+	// log.Printf("%v", f)
 	lambda.Start(HandleRequest)
 }
