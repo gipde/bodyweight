b51c65cb272a4c4a23ca053cf2aa80c93e9c38bc
Werner Schneider <werner@schneidernet.com>
2018-12-29 08:35:47 +0100

    test dynamodb

--- bodyweight.go
+++ bodyweight.go
@@ -3,9 +3,14 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"log"
 
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
 var result = []byte(`{
@@ -41,6 +46,48 @@ func HandleRequest(ctx context.Context, event interface{}) (interface{}, error) 
 	return f, err
 }
 
+type Item struct {
+	ID    string `json:"id"`
+	State int    `json:"state"`
+}
+
+func readDynamoDB() {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("eu-west-1")},
+	)
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+
+	result, err := svc.GetItem(&dynamodb.GetItemInput{
+		TableName: aws.String("bodyweight"),
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": {
+				N: aws.String("state"),
+			},
+		},
+	})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	item := Item{}
+
+	err = dynamodbattribute.UnmarshalMap(result.Item, &item)
+
+	if err != nil {
+		panic(fmt.Sprintf("Failed to unmarshal Record, %v", err))
+	}
+
+	fmt.Println("Found item:")
+	fmt.Println("id:  ", item.ID)
+	fmt.Println("State: ", item.State)
+
+}
+
 func main() {
+	readDynamoDB()
 	lambda.Start(HandleRequest)
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,6 @@
 module github.com/gipde/bodyweight
 
-require github.com/aws/aws-lambda-go v1.8.0
+require (
+	github.com/aws/aws-lambda-go v1.8.0
+	github.com/aws/aws-sdk-go v1.16.11
+)
